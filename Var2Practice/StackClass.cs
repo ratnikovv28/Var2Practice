355c62997714f4f0a5ce59a7ed99e5e629085147
Дмитрий Степанов <stepanov3.da@edu.spbstu.ru>
2021-03-20 16:23:39 +0300

    Создал стэк

--- Var2Practice/StackClass.cs
+++ Var2Practice/StackClass.cs
@@ -4,9 +4,65 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Var2Practice
+namespace Practice
 {
-    class StackClass
+    public class StackClass<T>
     {
+        private T[] items;
+        private int count;
+        const int n = 10;
+        public StackClass()
+        {
+            items = new T[n];
+        }
+
+        public bool IsEmpty
+        {
+            get
+            {
+                return count == 0;
+            }
+        }
+        public int Count
+        {
+            get
+            {
+                return count;
+            }
+        }
+
+        public void Push(T item)
+        {
+            // увеличиваем стек
+            if (count == items.Length)
+                Resize(items.Length + 10);
+
+            items[count++] = item;
+        }
+        public T Pop()
+        {
+            // если стек пуст, выбрасываем исключение
+            if (IsEmpty)
+                throw new InvalidOperationException("Стек пуст");
+            T item = items[--count];
+            items[count] = default(T); // сбрасываем ссылку
+
+            if (count > 0 && count < items.Length - 10)
+                Resize(items.Length - 10);
+
+            return item;
+        }
+        public T Peek()
+        {
+            return items[count - 1];
+        }
+
+        private void Resize(int max)
+        {
+            T[] tempItems = new T[max];
+            for (int i = 0; i < count; i++)
+                tempItems[i] = items[i];
+            items = tempItems;
+        }
     }
 }
